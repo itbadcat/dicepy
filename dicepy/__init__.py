c13424f88d065fe53ba4faa60bc50630dc0da399
Houser <houser@lincat.housin.net>
2024-07-14 11:06:08 -0500

    Fixed roll keep issue

--- dicepy/__init__.py
+++ dicepy/__init__.py
@@ -24,11 +24,11 @@ class Roll:
     def __init__(self, sides: int, dice_count: int, keep_type: KeepType = None, keep_amount: int = 1):
         self.dice = [randint(1, sides) for _ in range(dice_count)]
         self.keep_type = keep_type
+        self.keep_amount = keep_amount
         if keep_type == None:
             self.value = sum(self.dice)
         # 3d6k2 and 3d6kh2
         elif keep_type == KeepType.highest or keep_type == KeepType.keep:
-            self.keep_amount = 2
             self.value = sum(sorted(self.dice)[-self.keep_amount:])
         else: # KeepType.lowest
             self.value = sum(sorted(self.dice)[:self.keep_amount])
